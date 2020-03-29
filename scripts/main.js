086c635e0ef8aa8199dc3007ec074e68949fdded
SyureNyanko <dr.myself+github@gmail.com>
2020-03-29 12:29:47 +0900

    copy paste and modify for html

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+function Execute(){
+    const result = readText(
+        (text) => {
+            console.log(text);
+            writeText(text);
+        }
+    );
+}
+
+function process(input){
+    return input.replace(/\r?\n/g, '');
+}
+
+function writeText(text){
+    if(navigator.clipboard){
+        navigator.clipboard.writeText(process(text));
+    }
+}
+
+const readText = (callback) => {
+    if(navigator.clipboard) {
+        navigator.clipboard.readText()
+        .then(function(text){
+            callback(text);
+        });
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
